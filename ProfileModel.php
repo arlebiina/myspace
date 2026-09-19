128e0b2b87e4718535c1a90ab88b3e7fdb64ecc1
maria <102561475+arlebiina@users.noreply.github.com>
2026-09-18 23:26:35 -0300

    Update ProfileModel.php

--- ProfileModel.php
+++ ProfileModel.php
@@ -5,9 +5,9 @@ class ProfileModel {
         return [
             'name' => 'xXx...KandiThrasH...xXx',
             'myspace_url' => 'http://myspace.2kool4u.net',
-            'tagline' => "rawr means i ♥ you in dinosaur x3\n***\nKandi\n17 / Bi\nTaken\n02.14.08 <33\n***",
-            'last_login' => '6/7/2008',
-            'avatar' => './assets/images/perfil.jpg',
+            'tagline' => "rawr means i s2 you in dinosaur !\n\nKandi\n18 / Bi\nTaken\n02.14.08 <33\n***",
+            'last_login' => '6/7/2013',
+            'avatar' => './assets/images/gyaru.gif',
             'interests' => [
                 'General' => 'Music, hanging w/ friends, killin posers',
                 'Music' => 'BMTH, A7X, ADTR, MIW, BvB, TBS',
@@ -19,8 +19,8 @@ class ProfileModel {
     'friends' => [
     ['name' => 'black☆star 🌟', 'image' => './assets/images/blackstar.jpg', 'url' => 'https://souleater.fandom.com/wiki/Black%E2%98%86Star'],
     ['name' => 'denji 🪚',      'image' => './assets/images/denji.jpg',     'url' => 'https://chainsaw-man.fandom.com/wiki/Denji'],
-    ['name' => 'yui 🎸',        'image' => './assets/images/yui.jpg',       'url' => 'https://k-on.fandom.com/wiki/Yui_Hirasawa'],
-    ['name' => 'nefer 🐾',      'image' => './assets/images/nefer.jpg',     'url' => 'https://genshin-impact.fandom.com/wiki/Nefer'],
+    ['name' => 'yui 🎸',        'image' => './assets/images/aya.jpg',       'url' => 'https://k-on.fandom.com/wiki/Yui_Hirasawa'],
+    ['name' => 'arlecchino 🩸', 'image' => './assets/images/arlecchino.jpg',     'url' => 'https://genshin-impact.fandom.com/wiki/Arlecchino'],
     ['name' => 'mitsuri 🍡',   'image' => './assets/images/mitsuri.jpg',    'url' => 'https://kimetsu-no-yaiba.fandom.com/wiki/Mitsuri_Kanroji'],
     ['name' => 'maomao 🍃',    'image' => './assets/images/maomao.jpg',     'url' => 'https://kusuriya.fandom.com/wiki/Maomao'],
     ['name' => 'naruto 🍥',    'image' => './assets/images/naruto.jpg',     'url' => 'https://naruto.fandom.com/pt-br/wiki/Naruto_Uzumaki'],
